7fa99ea68742d77b144c8f1c5bf84195560068c6
拓也 横山 <ignisrt7mssp@gmail.com>
2019-09-20 22:29:59 +0900

    iboサイズの修正

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,32 @@
 *	@author	Takuya Yokoyama
 */
 
-#define GLEW_STATIC
-
 #include <iostream>
+#include <vector>
 #include <glm/glm.hpp>
 #include "GLSystem.h"
 #include "BufferObject.h"
+
+static const char* vsCode =
+"#version 410 \n"
+"layout(location=0) in vec3 vPosition;"
+"layout(location=1) in vec2 vTexcoord;"
+"layout(location=2) in vec4 vColor;"
+"layout(location=0) out vec4 outColor;"
+"void main() {"
+"  outColor = vColor;"
+"  gl_Position = vec4(vPosition, 1.0);"
+"}";
+
+/// フラグメントシェーダ.
+static const char* fsCode =
+"#version 410 \n"
+"layout(location=0) in vec4 inColor;"
+"out vec4 fragColor;"
+"void main() {"
+"  fragColor = inColor;"
+"}";
+
 
 /**
 *	頂点データ構造体
@@ -32,6 +52,78 @@ GLuint indices[] = {
 	0,1,2,2,3,0
 };
 
+/**
+* シェーダコードをコンパイルする.
+*
+* @param type シェーダの種類.
+* @param string シェーダコードへのポインタ.
+*
+* @return 作成したシェーダオブジェクト.
+*/
+GLuint CompileShader(GLenum type, const GLchar* string)
+{
+	GLuint shader = glCreateShader(type);
+	glShaderSource(shader, 1, &string, nullptr);
+	glCompileShader(shader);
+	GLint compiled = 0;
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
+	if (!compiled) {
+		GLint infoLen = 0;
+		glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infoLen);
+		if (infoLen) {
+			std::vector<char> buf;
+			buf.resize(infoLen);
+			if (static_cast<int>(buf.size()) >= infoLen) {
+				glGetShaderInfoLog(shader, infoLen, NULL, buf.data());
+				std::cerr << "ERROR: シェーダのコンパイルに失敗￥n" << buf.data() << std::endl;
+			}
+		}
+		glDeleteShader(shader);
+		return 0;
+	}
+	return shader;
+}
+
+/**
+* プログラムオブジェクトを作成する.
+*
+* @param vsCode 頂点シェーダコードへのポインタ.
+* @param fsCode フラグメントシェーダコードへのポインタ.
+*
+* @return 作成したプログラムオブジェクト.
+*/
+GLuint CreateShaderProgram(const GLchar* vsCode, const GLchar* fsCode)
+{
+	GLuint vs = CompileShader(GL_VERTEX_SHADER, vsCode);
+	GLuint fs = CompileShader(GL_FRAGMENT_SHADER, fsCode);
+	if (!vs || !fs) {
+		return 0;
+	}
+	GLuint program = glCreateProgram();
+	glAttachShader(program, fs);
+	glDeleteShader(fs);
+	glAttachShader(program, vs);
+	glDeleteShader(vs);
+	glLinkProgram(program);
+	GLint linkStatus = GL_FALSE;
+	glGetProgramiv(program, GL_LINK_STATUS, &linkStatus);
+	if (linkStatus != GL_TRUE) {
+		GLint infoLen = 0;
+		glGetProgramiv(program, GL_INFO_LOG_LENGTH, &infoLen);
+		if (infoLen) {
+			std::vector<char> buf;
+			buf.resize(infoLen);
+			if (static_cast<int>(buf.size()) >= infoLen) {
+				glGetProgramInfoLog(program, infoLen, NULL, buf.data());
+				std::cerr << "ERROR: シェーダのリンクに失敗￥n" << buf.data() << std::endl;
+			}
+		}
+		glDeleteProgram(program);
+		return 0;
+	}
+	return program;
+}
+
 int main(){
 
 	GLSystem::Window& window = GLSystem::Window::Instance();
@@ -39,29 +131,46 @@ int main(){
 		return -1;
 	}
 
-
 	auto i = glGetError();
 
 	BufferObject vbo, ibo;
 	VertexArrayObject vao;
 	vbo.Init("VertexBuffer", GL_ARRAY_BUFFER, sizeof(vertices), vertices);
-	ibo.Init("IndexBuffer", GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint), indices);
+	ibo.Init("IndexBuffer", GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices);
 	vao.Init(vbo.GetId(), ibo.GetId());
 
-	if (vao.Bind()) {
+	if (!vbo.isValid() || !ibo.isValid() || !vao.isValid()) {
+		return -1;
+	}
 
+	if (vao.Bind()) {
 		vao.VertexAttribPointer(0, 3, GL_FLOAT, sizeof(Vertex), reinterpret_cast<GLvoid*>(offsetof(Vertex, position)));
 		vao.VertexAttribPointer(1, 2, GL_FLOAT, sizeof(Vertex), reinterpret_cast<GLvoid*>(offsetof(Vertex, texcoord)));
 		vao.VertexAttribPointer(2, 4, GL_FLOAT, sizeof(Vertex), reinterpret_cast<GLvoid*>(offsetof(Vertex, color)));
 		vao.UnBind(true);
 	}
 
+	GLuint program = CreateShaderProgram(vsCode, fsCode);
+	if (!program) {
+		return -1;
+	}
 
 	//main loop
 	while (!window.ShouldClose()) {
 
 		glClearColor(0.8f, 0.8f, 0.8f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		glUseProgram(program);
+		if (vao.Bind()) {
+
+			glDrawArrays(GL_TRIANGLES, 0, sizeof(vertices) / sizeof(vertices[0]));
+			glDrawElements(
+				GL_TRIANGLES, sizeof(indices) / sizeof(indices[0]),
+				GL_UNSIGNED_INT, reinterpret_cast<const GLvoid*>(0));
+
+			vao.UnBind();
+		}
 
 		window.SwapBuffers();
 	}
