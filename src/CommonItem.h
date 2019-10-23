4779f8844c14442894c0405b603d1bbf8aa37bde
拓也 横山 <ignisrt7mssp@gmail.com>
2019-10-23 21:24:24 +0900

    共通項目の定義場所を作成

---
+++ src/CommonItem.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace WindowParameter{
+
+	static const int windowSizeWidth = 1000;		///< ウインドウの幅
+	static const int windowSizeHeight = 800;		///< ウインドウの高さ
+	static const char* windowName = "OpenGL3D ActionGame";	///< ウインドウ名
+}
+
+namespace FileSystemPass{
+
+	
+}
