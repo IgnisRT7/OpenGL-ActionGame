a08c0836fa9ae3456dba423193e88f923d35f692
拓也 横山 <ignisrt7mssp@gmail.com>
2019-12-10 23:45:51 +0900

    リソースファイル管理用の名前空間作成

---
+++ src/Resource.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace ShaderFile {
+	static const char* finalRender_vert = "res/shader/FinalRender.vert";
+	static const char* finalRender_frag = "res/shader/FinalRender.frag";
+	static const char* default_vert = "res/shader/Default.vert";
+	static const char* default_frag = "res/shader/Default.frag";
+	static const char* fontRenderer_vert = "res/shader/FontRenderer.vert";
+	static const char* fontRenderer_frag = "res/shader/FontRenderer.frag";
+}
