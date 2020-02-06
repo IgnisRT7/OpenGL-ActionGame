84e7a6814ecb3f96395e0edf5611418a4c2bcd72
IgnisRT7 <35602066+IgnisRT7@users.noreply.github.com>
2020-02-06 12:10:00 +0900

    各種ファイルパスの追加

--- src/Resource.h
+++ src/Resource.h
@@ -8,9 +8,17 @@ namespace ShaderFile {
 	static const char* default_frag = "res/shader/Default.frag";
 	static const char* fontRenderer_vert = "res/shader/FontRenderer.vert";
 	static const char* fontRenderer_frag = "res/shader/FontRenderer.frag";
+	static const char* staticMesh_vert = "res/shader/StaticMesh.vert";
+	static const char* staticMesh_frag = "res/shader/StaticMesh.frag";
 }
 
 namespace TextureFile {
 
 	static const char* testDebugTexture = "res/texture/sampleTex.dds";
+	static const char* testDebugTexture2 = "res/texture/sampleTex2.dds";
+}
+
+namespace MeshFile {
+
+	static const char* treeModel = "res\mesh\red_pine_tree\red_pine_tree.bin";
 }
